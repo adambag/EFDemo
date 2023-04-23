6432f362c6801ad850c0031239b3f8e588625b4f
Adam Bagiński <abaginski2001@gmail.com>
2023-04-23 17:46:56 +0200

    Dodaj pliki projektów.

---
+++ Models/Group.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EFDemo.Models
+{
+    public class Group
+    {
+        public int Id { get; set; }
+        [Required]
+        [MaxLength(100)]
+        public string Name { get; set; }
+        public ICollection<PersonGroup>? PersonGroups { get; set; }
+    }
+
+}
---
+++ Models/PersonGroup.cs
@@ -0,0 +1,10 @@
+﻿namespace EFDemo.Models
+{
+    public class PersonGroup
+    {
+        public int PersonId { get; set; } //klucz obcy do Person
+        public Person Person { get; set; }
+        public int GroupId { get; set; } //klucz obcy do Group
+        public Group Group { get; set; }
+    }
+}
